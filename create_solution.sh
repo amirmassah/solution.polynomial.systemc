088c5609bcbe8bd7963d6b988a8ec159109e5b78
Christian De Schryver <schryver@eit.uni-kl.de>
2016-05-25 09:00:27 +0200

    adapted to global solution settings

--- create_solution.sh
+++ create_solution.sh
@@ -1,8 +1,8 @@
-source "settings/settings.sh"
+source "../solution.settings/settings.sh"
 export REPO_NAME="polynomial.systemc"
 
 ./clean.sh
 git clone $GITHUB_PREFIX$REPO_NAME".git"
 
 mv $REPO_NAME solution
-cp files/* -r solution/
+cp -r files/* solution/
